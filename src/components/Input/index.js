4878b0f6bc82998c4b8246052f98bc935c227518
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-18 16:49:08 -0300

    create component input

---
+++ src/components/Input/index.js
@@ -0,0 +1,31 @@
+import { useState, useRef } from "react";
+import * as S from "./styles";
+
+const Input = ({ icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  };
+
+  return (
+    <S.Container isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon />}
+      <input
+        {...rest}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+      />
+    </S.Container>
+  );
+};
+
+export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,44 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  background-color: #333;
+  border: 2px solid #333;
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  border-radius: 0.5rem;
+
+  & + div {
+    margin-top: 0.8rem;
+  }
+
+  > svg {
+    font-size: 1.5rem;
+    color: #939393;
+  }
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
+  ${(props) =>
+    props.isFilled &&
+    css`
+      svg {
+        color: #ff9000;
+      }
+    `}
+
+  input {
+    padding: 0 0.8rem;
+    width: 100%;
+    background-color: transparent;
+    border: 0;
+    font-size: 1.1rem;
+    outline: 0;
+    color: #f1f1f1;
+  }
+`;
