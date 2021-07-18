66b0a30e951d5ec23617af9cc85a1c7203cabf04
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-18 16:49:29 -0300

    create button for login gmail

---
+++ src/components/ButtonGmail/index.js
@@ -0,0 +1,13 @@
+import { SiGmail } from "react-icons/si";
+import * as S from "./styles";
+
+const ButtonGmail = () => {
+  return (
+    <S.Container>
+      <SiGmail />
+      Entrar com o gmail
+    </S.Container>
+  );
+};
+
+export default ButtonGmail;
---
+++ src/components/ButtonGmail/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  width: 20rem;
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.1rem;
+  background-color: #e04343;
+  color: #fff;
+  border-radius: 0.5rem;
+  transition: all 0.3s;
+
+  > svg {
+    margin-right: 1rem;
+    font-size: 1.3rem;
+  }
+
+  :hover {
+    filter: brightness(0.9);
+
+    > svg {
+      transform: rotate(360deg);
+      transition: 1s;
+    }
+  }
+`;
