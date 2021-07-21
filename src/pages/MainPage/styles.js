80fa9996811614d7216ada668399509f7cc366e8
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-20 21:39:40 -0300

    add main page

--- src/pages/MainPage/styles.js
+++ src/pages/MainPage/styles.js
@@ -1,5 +1,56 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  height: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+
+  > h2 {
+    margin-top: 0.8rem;
+    text-align: center;
+    font-family: 'Poppins', sans-serif;
+
+    @media (max-width: 500px) {
+      margin-left: 2rem;
+      margin-top: 2rem;
+
+      text-align: left;
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  margin-top: 2rem;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+
+  @media (max-width: 500px) {
+    width: 19rem;
+    margin-left: 2rem;
+    text-align: left;
+    font-size: 1.7rem;
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 2.5rem;
+  max-height: 410px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2.5rem;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+  margin-left: 1rem;
+
+  ::-webkit-scrollbar {
+    width: 0;
+  }
+
+  @media (max-width: 500px) {
+    justify-content: flex-start;
+    align-items: none;
+  }
 `;
