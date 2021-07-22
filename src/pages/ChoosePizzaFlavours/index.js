fbae282c2fb2888ba375bd6dbca67246d98175ca
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-21 23:05:03 -0300

    create page  choose pizza flavours

--- src/pages/ChoosePizzaFlavours/index.js
+++ src/pages/ChoosePizzaFlavours/index.js
@@ -1,30 +1,61 @@
+import { useState } from 'react';
+
+import CardPizzaFlavours from 'components/CardPizzaFlavours';
+import Carousel from 'components/Carosel';
+import { Title, Center } from 'Ui';
 import { useAuth } from 'hooks/useAuth';
-import CardSizePizza from 'components/CardSizePizza';
-import { dataChoosePizzaSize } from 'fakes/choosePizzaSize';
-import { CHOOSE_PIZZA_FLAVOURS } from 'router/routes';
+import { CHOOSE_PIZZA_FLAVOURS, HOME } from 'router/routes';
+import { pizzasFlavours } from 'fakes/pizzasFlavours';
 import * as S from './styles';
+import { Redirect } from 'react-router-dom';
 
 const ChoosePizzaFlavours = ({ location }) => {
+  const [checkboxes, setCheckBoxes] = useState({});
   const { user } = useAuth();
 
-  return (
-    <S.Container>
-      <S.Title>Escolha o sabor da pizza</S.Title>
+  if (!location.state) return <Redirect to={HOME} />;
+  const { flavours, id } = location.state;
 
-      <S.Content>
-        {dataChoosePizzaSize.map(pizza => (
-          <CardSizePizza
-            to={CHOOSE_PIZZA_FLAVOURS}
-            // isActive={}
-            key={pizza.id}
-            size={pizza.size}
-            title={pizza.name}
-            slice={pizza.slices}
-            flavorQtd={pizza.flavours}
-          />
+  const checkboxesChecked = checkboxes => {
+    return Object.values(checkboxes).filter(Boolean);
+  };
+
+  const handleChangeCheckBox = (event, pizzaId) => {
+    if (
+      checkboxesChecked(checkboxes).length === flavours &&
+      event.target.checked
+    )
+      return;
+
+    setCheckBoxes(prevState => ({
+      ...prevState,
+      [pizzaId]: event.target.checked,
+    }));
+  };
+
+  return (
+    <Center>
+      <Title>{`Escolha até ${flavours} sabores`}</Title>
+      <Carousel>
+        {pizzasFlavours.map(pizza => (
+          <label key={pizza.id}>
+            <S.Checkbox
+              type="checkbox"
+              checked={!!checkboxes[pizza.id]}
+              onChange={e => handleChangeCheckBox(e, pizza.id)}
+            />
+
+            <CardPizzaFlavours
+              to={CHOOSE_PIZZA_FLAVOURS}
+              isActive={!!checkboxes[pizza.id]}
+              name={pizza.name}
+              value={pizza.value[id]}
+              image={pizza.image}
+            />
+          </label>
         ))}
-      </S.Content>
-    </S.Container>
+      </Carousel>
+    </Center>
   );
 };
 
