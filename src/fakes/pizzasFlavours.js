8b04894715f03e336036b3ceeb98d813b7f9b9b1
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-21 23:03:58 -0300

    add data facke

---
+++ src/fakes/pizzasFlavours.js
@@ -0,0 +1,46 @@
+import pizza01 from 'assets/pizza-calabresa.png';
+import pizza02 from 'assets/pizza-milho-bacon.png';
+import pizza03 from 'assets/pizza-toscana.png';
+
+export const pizzasFlavours = [
+  {
+    id: 0,
+    image: pizza01,
+    name: 'Calabresa',
+    value: {
+      0: 20,
+      1: 30,
+      2: 45,
+    },
+  },
+  {
+    id: 1,
+    image: pizza02,
+    name: 'Milho com Bacon',
+    value: {
+      0: 25,
+      1: 36,
+      2: 48,
+    },
+  },
+  {
+    id: 2,
+    image: pizza03,
+    name: 'Toscana',
+    value: {
+      0: 28,
+      1: 40,
+      2: 51,
+    },
+  },
+  {
+    id: 3,
+    image: pizza02,
+    name: 'Bacon',
+    value: {
+      0: 20,
+      1: 32,
+      2: 38,
+    },
+  },
+];
