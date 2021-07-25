cd6db2377e09b85b34cd594496f960053c6eedcf
mauricio girardi <mauriciogirardi@denox.com.br>
2021-07-25 15:59:33 -0300

    add route

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,3 +3,5 @@ export const LOGIN = '/login';
 export const CHOOSE_PIZZA_FLAVOURS = '/escolher-sabores';
 export const CHOOSE_PIZZA_QUANTITY = '/escolher-quantidade';
 export const CHECKOUT = '/finalizar';
+export const CHECKOUT_CONFIRMATION = '/checkout';
+export const CHECKOUT_FINISH = '/checkout-finalizado';
